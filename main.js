7f9ce7c455489e294f492577b876579a6d46db27
SharQawy Wael <124384551+SharQawy1@users.noreply.github.com>
2023-05-26 18:48:21 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 //3ama
+
 let aamadownDate = new Date("June 12, 2023 00:00:00");
 
 let counter1 = setInterval(() => {
   let dateNow = new Date().getTime();
   let dateDIff = aamadownDate - dateNow;
+  if (dateDIff < 0) {
+    document.querySelector(
+      ".aama .title"
+    ).innerHTML = `لقد بدأت امتحانات الثانوية العامة وستنتهي بعد`;
+    dateDIff = new Date("July 13, 2023 00:00:00") - dateNow;
+  }
   let days = Math.floor(dateDIff / 1000 / 60 / 60 / 24);
   let hours = Math.floor((dateDIff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   let mins = Math.floor((dateDIff % (1000 * 60 * 60)) / (1000 * 60));
@@ -12,7 +19,7 @@ let counter1 = setInterval(() => {
   document.querySelector(".hours1").innerHTML = hours;
   document.querySelector(".mins1").innerHTML = mins;
   document.querySelector(".secs1").innerHTML = secs;
-}, 1000);
+}, 10);
 
 // Azhar
 let azhardownDate = new Date("June 3, 2023 00:00:00");
@@ -20,6 +27,12 @@ let azhardownDate = new Date("June 3, 2023 00:00:00");
 let counter2 = setInterval(() => {
   let dateNow = new Date().getTime();
   let dateDIff = azhardownDate - dateNow;
+  if (dateDIff < 0) {
+    document.querySelector(
+      ".aama .title"
+    ).innerHTML = `لقد بدأت امتحانات الثانوية الأزهرية وستنتهي بعد`;
+    dateDIff = new Date("July 12, 2023 00:00:00") - dateNow;
+  }
   let days = Math.floor(dateDIff / 1000 / 60 / 60 / 24);
   let hours = Math.floor((dateDIff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   let mins = Math.floor((dateDIff % (1000 * 60 * 60)) / (1000 * 60));
@@ -28,4 +41,4 @@ let counter2 = setInterval(() => {
   document.querySelector(".hours2").innerHTML = hours;
   document.querySelector(".mins2").innerHTML = mins;
   document.querySelector(".secs2").innerHTML = secs;
-}, 1000);
+}, 10);
